f28e351ef2fc5fe57634e46fd345f7a7b454d994
Theo <sagniez.theo@laposte.net>
2022-07-05 13:28:06 +0200

    Created slide in menu

--- scripts.js
+++ scripts.js
@@ -1,5 +1,30 @@
-const hamburger = document.querySelector('.bi-list')
+const menu = document.querySelector('.menu')
+const hamburger = document.querySelector('.hamburgers')
+const lines = document.querySelectorAll('.hamburger')
 
 hamburger.addEventListener('click', () => {
-    alert('Clicked hamburger')
+    if (!menu.classList.contains('slideIn') && 
+        !lines[0].classList.contains('tipDown') && 
+        !lines[1].classList.contains('fadeOut') && 
+        !lines[2].classList.contains('tipUp')) {
+            menu.classList.remove('slideOut')
+            lines[0].classList.remove('revTipDown')
+            lines[1].classList.remove('fadeIn')
+            lines[2].classList.remove('revTipUp')
+
+            menu.classList.add('slideIn')
+            lines[0].classList.add('tipDown')
+            lines[1].classList.add('fadeOut')
+            lines[2].classList.add('tipUp')
+    } else {
+        menu.classList.remove('slideIn')
+        lines[0].classList.remove('tipDown')
+        lines[1].classList.remove('fadeOut')
+        lines[2].classList.remove('tipUp')
+
+        menu.classList.add('slideOut')
+        lines[0].classList.add('revTipDown')
+        lines[1].classList.add('fadeIn')
+        lines[2].classList.add('revTipUp')
+    }
 })
